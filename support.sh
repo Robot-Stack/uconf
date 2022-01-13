a2839e87394e6fe05c7282b7bace7c04b525cf84
GrindWhale <grindwhale@gmail.com>
2022-01-13 19:09:36 +0100

    Update support.sh

--- support.sh
+++ support.sh
@@ -10,6 +10,7 @@ function spause()
 
 function ReConfigureBashRc()
 {
+    echo "export PATH=\$PATH:$ScriptDir to $HOME/.bashrc"
     AppendStringOnce "export PATH=\$PATH:$ScriptDir" "$HOME/.bashrc"
 }
 
