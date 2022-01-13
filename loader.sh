b61aac38930dd82d8c9d5f9eed6eecc17f25ee12
GrindWhale <grindwhale@gmail.com>
2022-01-13 18:54:40 +0100

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -3,6 +3,6 @@
 ScriptDir="$(realpath "$0" | sed 's|\(.*\)/.*|\1|')"
 wget -q --no-cache --no-cookies  -O $ScriptDir/generic.sh https://raw.githubusercontent.com/NaitWatch/uconf/main/generic.sh
 wget -q --no-cache --no-cookies  -O $ScriptDir/support.sh https://raw.githubusercontent.com/NaitWatch/uconf/main/support.sh
-wget -q --no-cache --no-cookies  -O $ScriptDir/uconf.sh https://raw.githubusercontent.com/NaitWatch/uconf/main/main.sh
+wget -q --no-cache --no-cookies  -O $ScriptDir/uconf.sh https://raw.githubusercontent.com/NaitWatch/uconf/main/uconf.sh
 chmod +x "$ScriptDir/uconf.sh"
 bash "$ScriptDir/uconf.sh"
